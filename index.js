ae1f3ad8d08084a3d573b8398ed67e9231fd0da3
Southstar <Akinbileoluwaseyi2001@gmail.com>
2023-07-24 22:41:22 +0100

    FIrst render on index.js

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 import { menuArray } from "./data.js";
 
+function render(){
+    const main = document.getElementById('main')
+    let htmlstring = ''
+    menuArray.forEach(
+        (x) => {
+            htmlstring += `<div class="item">
+
+            <span class="icon">${x.emoji}</span>
+            <div class="descrip">
+                <span class="name">${x.name}</span>
+                <span class="ingredient">${x.ingredients}</span>
+                <span class="price">$${x.price}</span>
+            </div>
+            <button>+</button>
+
+        </div>
+`
+        }
+    )
+    main.innerHTML = htmlstring
+}
+
+render()
