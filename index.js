1651eeb3f43aafb0ed4dca4e799c9c69fa5c427b
Southstar <Akinbileoluwaseyi2001@gmail.com>
2023-07-25 12:29:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
 import { menuArray } from "./data.js";
 
-let orderList = ""
-
+let orderList = []
 
 document.addEventListener("click", (x) =>{
 
         if (x.target.dataset.add) {
        makeOrder(x.target.dataset.add)
-       render()
         }
-})
-
-function makeOrder(x) {
-    const order = menuArray.filter((z)=> {
-        return z.id == x
-    })[0]
-
-    orderList += `           
-    <div class="order">
-
-    <div class="order-name">
-        <span>${order.name}</span>
-        <button>Remove</button>
-    </div>
-
-    <span class="order-price">
-    ${order.price}
-    </span>
-
-    </div>
-    `    
-}
+        else if (x.target.dataset.remove) {
+        RemoveOrder(x.target.dataset.remove)
+        }
+        render()
+    })
 
 function availableOrders() {
     let htmlstring = ''
@@ -53,12 +34,51 @@ function availableOrders() {
     return htmlstring
 }
 
+function makeOrder(x) {
+
+    const order = menuArray.filter((z)=> {
+        return z.id == x
+    })[0]
+
+    orderList.push(order)
+}
+
+function RemoveOrder(x) {
+   const num = orderList.filter((z) => {
+        return z.id == x
+    })[0]
+    orderList.splice(orderList.indexOf(num))
+}
+
+function renderOrder(){
+    let htmlstring = ''
+    orderList.forEach( (x) => {
+            htmlstring +=     `           
+            <div class="order">
+        
+            <div class="order-name">
+                <span>${x.name}</span>
+                <button data-remove="${x.id}">Remove</button>
+            </div>
+        
+            <span class="order-price">
+            $${x.price}
+            </span>
+        
+            </div>
+            `    
+        
+        }
+    )
+    return htmlstring
+}
+
 function render(){
     const main = document.getElementById('main')
     const order = document.getElementById('order-list')
     main.innerHTML = availableOrders()
-    if (order){
-    order.innerHTML = orderList
+    if (orderList){
+    order.innerHTML = renderOrder()
     }
 }
 
