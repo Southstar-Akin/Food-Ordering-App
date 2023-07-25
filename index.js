49b9d8330910871393b4530b4d04938d7f45f1bc
Southstar <Akinbileoluwaseyi2001@gmail.com>
2023-07-25 11:29:24 +0100

    Js update( Functionality)
    Functionality added

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 import { menuArray } from "./data.js";
 
-function render(){
-    const main = document.getElementById('main')
+let orderList = ""
+
+
+document.addEventListener("click", (x) =>{
+
+        if (x.target.dataset.add) {
+       makeOrder(x.target.dataset.add)
+       render()
+        }
+})
+
+function makeOrder(x) {
+    const order = menuArray.filter((z)=> {
+        return z.id == x
+    })[0]
+
+    orderList += `           
+    <div class="order">
+
+    <div class="order-name">
+        <span>${order.name}</span>
+        <button>Remove</button>
+    </div>
+
+    <span class="order-price">
+    ${order.price}
+    </span>
+
+    </div>
+    `    
+}
+
+function availableOrders() {
     let htmlstring = ''
     menuArray.forEach(
         (x) => {
@@ -13,13 +44,22 @@ function render(){
                 <span class="ingredient">${x.ingredients}</span>
                 <span class="price">$${x.price}</span>
             </div>
-            <button>+</button>
+            <button data-add="${x.id}">+</button>
 
         </div>
 `
         }
     )
-    main.innerHTML = htmlstring
+    return htmlstring
+}
+
+function render(){
+    const main = document.getElementById('main')
+    const order = document.getElementById('order-list')
+    main.innerHTML = availableOrders()
+    if (order){
+    order.innerHTML = orderList
+    }
 }
 
 render()
